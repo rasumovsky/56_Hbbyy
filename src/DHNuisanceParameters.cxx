d75b057f490e61fa8049e9100a2360dc45f9a5f7
Andrew Hard <hardandrew1@gmail.com>
2016-02-25 15:48:56 +0100

    NP ranking and pulling runs

--- src/DHNuisanceParameters.cxx
+++ src/DHNuisanceParameters.cxx
@@ -49,174 +49,305 @@ int main(int argc, char **argv) {
 			   (config->getStr("JobName")).Data());
   system(Form("mkdir -vp %s", outputDir.Data()));
   
-  // Define the input file, then make a local copy (for remote jobs):
-  TString originFile = Form("%s/%s/DHWorkspace/rootfiles/workspaceDH_%s.root",
-			    (config->getStr("MasterOutput")).Data(), 
-			    jobName.Data(), anaType.Data());
-  
   // Create a nametag for results:
   TString tag = (anaType.EqualTo("Resonant")) ? 
     Form("ResonantMX%d", resonanceMass) : "NonResonant";
   
   // Set the plot Style to ATLAS defaults:
   CommonFunc::SetAtlasStyle();
+
+  // Store (unranked) names of nuisance parameters:
+  std::vector<TString> paramNames; paramNames.clear();
   
-  // Open the workspace:
-  TFile wsFile(originFile, "read");
-  RooWorkspace *workspace = (RooWorkspace*)wsFile.Get("combinedWS");
+  // Store impact on mu:
+  double impactRank[100] = {0};
+  double impactRankErr[100] = {0.5};
+  double points[100] = {0.0};
+  double muLo[100] = {0.0};
+  double muHi[100] = {0.0};
   
-  // Instantiate the test statistic class for calculations and plots:
-  DHTestStat *dhts = new DHTestStat(configFile, "new", workspace);
-    
+  // Store the NP info for the mu=0 fit:
+  double valMu0[100] = {0.0};
+  double errLoMu0[100] = {0.0};
+  double errHiMu0[100] = {0.0};
+  
+  // Store the NP info for the mu=1 fit:
+  double valMu1[100] = {0.0};
+  double errLoMu1[100] = {0.0};
+  double errHiMu1[100] = {0.0};
+  
+  // Store the NP info for the mu=free fit:
+  double valMuFree[100] = {0.0};
+  double errLoMuFree[100] = {0.0};
+  double errHiMuFree[100] = {0.0};
+  
+  TString fileNameNP = Form("%s/nuisance_parameters_%s.txt", 
+			    outputDir.Data(), tag.Data());
+  
+  // Keep track of whether all fits work:
+  bool allGoodFits = true;
+  
   //----------------------------------------//
   // Open nuisance parameter values from file:
   if (options.Contains("FromFile")) {
     
     // Open the saved CL values from asymptotics:
-    TString inputNPName = Form("%s/nuisanceParameterValues_%s.txt",
-				 outputDir.Data(), tag.Data());
-    ifstream inputFile(inputNPName);
+    
+    ifstream inputFile(fileNameNP);
     if (inputFile.is_open()) {
-      while (inputFile >> varValues_asym[n_asym] >> CLObs_asym[n_asym] 
-	     >> CLExp_asym[n_asym] >> CLExp_asym_p2[n_asym] 
-	       >> CLExp_asym_p1[n_asym] >> CLExp_asym_n1[n_asym] 
-	       >> CLExp_asym_n2[n_asym]) {
-	  n_asym++;
-	}
+      int i_n = 0; TString currName = "";
+      while (inputFile >> currName >> points[i_n] >> muLo[i_n] >> muHi[i_n]
+	     >> valMu0[i_n] >> errLoMu0[i_n] >> errHiMu0[i_n] >> valMu1[i_n]
+	     >> errLoMu1[i_n] >> errHiMu1[i_n] >> valMuFree[i_n] 
+	     >> errLoMuFree[i_n] >> errHiMuFree[i_n]) {
+	paramNames.push_back(currName);
+	i_n++;
       }
-      else {
-	std::cout << "DHNuisanceParameters: ERROR opnening file " 
-		  << inputNPName << std::endl;
-	exit(0);
-      }
-      
-      inputFile_asym.close();
+      inputFile.close();
+    }
+    else {
+      std::cout << "DHNuisanceParameters: ERROR opnening file " 
+		<< fileNameNP << std::endl;
+      exit(0);
     }
   }
   
   //----------------------------------------//
   // Calculate CL values from scratch and save them:
   else {
-    // Save values for plotting again!
-    ofstream outFile_asym.open(Form("%s/scan_CLvalues_asym_%s.txt", 
-				    outputDir.Data(), tag.Data()));
-          
+    // Open the workspace:
+    TString originFile = Form("%s/%s/DHWorkspace/rootfiles/workspaceDH_%s.root",
+			      (config->getStr("MasterOutput")).Data(), 
+			      jobName.Data(), anaType.Data());
+    TFile wsFile(originFile, "read");
+    RooWorkspace *workspace = (RooWorkspace*)wsFile.Get("combinedWS");
+    
+    // Instantiate the test statistic class for calculations and plots:
+    DHTestStat *dhts = new DHTestStat(configFile, "new", workspace);
+    
+    // Get the name of the dataset for fitting:
+    TString dataset = config->getStr("RankNPData");
+    
     // Set the value of the variable to scan:
-    dhts->setParam(config->getStr("CLScanVar"), crossSection, true);
+    dhts->setParam("xsec_SigBSM2H", config->getNum("RankNPCrossSection"), true);
     
     // If doing the resonant analysis, also set the res mass value:
     if (anaType.EqualTo("Resonant")) {
-      dhts->setParam("mResonance", resonanceMass, true);
+      dhts->setParam("mResonance", config->getNum("RankNPResonanceMass"), true);
     }
-  
-    // Keep nuisance parameter values after fitting (so that they can be saved):
-    dhts->resetParamsAfterFit(true);
     
-    //----------------------------------------//
-    // Storage for nuisance parameter data:
-    std::vector<TString> paramNames; paramNames.clear();
-    RooArgSet *nuisParameters = dhts->theModelConfig()->GetNuisanceParameters();
+    // Keep nuisance parameter values after fitting (so that they can be saved):
+    dhts->resetParamsAfterFit(false);
+    
+    // Create an (ordered) vector of nuisance parameter names:
+    const RooArgSet *nuisParameters 
+      = dhts->theModelConfig()->GetNuisanceParameters();
     TIterator *iterNuis = nuisParameters->createIterator();
     RooRealVar *currNuis = NULL;
     while ((currNuis = (RooRealVar*)iterNuis->Next())) {
       paramNames.push_back((TString)(currNuis->GetName()));
     }
     
-    double points[100] = {0.0};
-    double muLo[100] = {0.0};
-    double muHi[100] = {0.0};
-    
-    double valMu0[100] = {0.0};
-    double errLoMu0[100] = {0.0};
-    double errHiMu0[100] = {0.0};
-    
-    double valMu1[100] = {0.0};
-    double errLoMu1[100] = {0.0};
-    double errHiMu1[100] = {0.0};
-    
-    double valMuFree[100] = {0.0};
-    double errLoMuFree[100] = {0.0};
-    double errHiMuFree[100] = {0.0};
-    
     //----------------------------------------//
     // First get NP values from profiling data:
-    
+    std::cout << "DHNuisanceParameters: get NP values from profiling."
+	      << std::endl;
     double profiledPOIVal = -999.0;
     // Mu = 0 fits:
-    double nllMu0 = dhts->getFitNLL(m_dataToPlot, 0, true, profiledPOIVal);
-    if (!dhts->fitsAllConverged()) m_allGoodFits = false;
-    else {
-      RooArgSet *setMu0 = dhts->theModelConfig()->GetNuisanceParameters();
+    double nllMu0 = dhts->getFitNLL(dataset, 0, true, profiledPOIVal);
+    for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
+      if (dhts->theWorkspace()->var(paramNames[i_n])) {
+	valMu0[i_n] = dhts->theWorkspace()->var(paramNames[i_n])->getVal();
+	errLoMu0[i_n] =dhts->theWorkspace()->var(paramNames[i_n])->getError();
+	errHiMu0[i_n] =dhts->theWorkspace()->var(paramNames[i_n])->getError();
+      }
+      else {
+	std::cout << "DHNuisanceParameters: parameter " << paramNames[i_n]
+		  << " not found." << std::endl;
+      }
     }
     
     // Mu = 1 fits:
-    double nllMu1 = dhts->getFitNLL(m_dataToPlot, 1, true, profiledPOIVal);
-    if (!dhts->fitsAllConverged()) m_allGoodFits = false;
-    else {
-      RooArgSet *setMu1 = dhts->theModelConfig()->GetNuisanceParameters();
+    double nllMu1 = dhts->getFitNLL(dataset, 1, true, profiledPOIVal);
+    for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
+      if (dhts->theWorkspace()->var(paramNames[i_n])) {
+	valMu1[i_n] = dhts->theWorkspace()->var(paramNames[i_n])->getVal();
+	errLoMu1[i_n] =dhts->theWorkspace()->var(paramNames[i_n])->getError();
+	errHiMu1[i_n] =dhts->theWorkspace()->var(paramNames[i_n])->getError();
+      }
+      else {
+	std::cout << "DHNuisanceParameters: parameter " << paramNames[i_n]
+		  << " not found." << std::endl;
+      }
     }
-
-    // Mu free fits:
-    double nllMuFree = dhts->getFitNLL(m_dataToPlot, 1, false, profiledPOIVal);
-    if (!dhts->fitsAllConverged()) m_allGoodFits = false;
-    else {
-      RooArgSet *setMuFree = dhts->theModelConfig()->GetNuisanceParameters();
-    }
-        
     
+    // Mu free fits:
+    double nllMuFree = dhts->getFitNLL(dataset, 1, false, profiledPOIVal);
+    for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
+      if (dhts->theWorkspace()->var(paramNames[i_n])) {
+	valMu1[i_n] = dhts->theWorkspace()->var(paramNames[i_n])->getVal();
+	errLoMu1[i_n] =dhts->theWorkspace()->var(paramNames[i_n])->getError();
+	errHiMu1[i_n] =dhts->theWorkspace()->var(paramNames[i_n])->getError();
+      }
+      else {
+	std::cout << "DHNuisanceParameters: parameter " << paramNames[i_n]
+		  << " not found." << std::endl;
+      }
+    }
     
     //----------------------------------------//
     // Then get the impact of each NP on the signal strength:
-    
+    std::cout << "DHNuisanceParameters: get NP impact on signal strength."
+	      << std::endl;
     // First get the full uncertainty on mu:
     std::vector<TString> nullSet; nullSet.clear();
     std::map<int,double> mlScan
-      = dhts->scanNLL("mlScan", config->getStr("DataToScan"),
-		      config->getStr("ParamToScan"), nullSet);
+      = dhts->scanNLL("mlScan", dataset, config->getStr("RankNPPoI"), nullSet);
     
     // Loop over the NP:
     for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
       
       // Set the current parameter constant in the fit:
       std::vector<TString> currParamsToFix;
-      currParamToFix.clear();
+      currParamsToFix.clear();
       currParamsToFix.push_back(paramNames[i_n]);
       
       // Get the uncertainty on mu from this fit:
       std::map<int,double> currScan
-	= dhts->scanNLL("currScan", config->getStr("DataToScan"),
-			config->getStr("ParamToScan"), currParamsToFix);
+	= dhts->scanNLL("currScan", dataset, config->getStr("RankNPPoI"),
+			currParamsToFix);
       
       // Take the difference with usual error as uncertainty on mu from this NP:
       muLo[i_n] = -1.0 * fabs(mlScan[-1] - currScan[-1]);
       muHi[i_n] = fabs(currScan[1] - mlScan[1]);
       points[i_n] = i_n;
     }
+    allGoodFits = dhts->fitsAllConverged();
 
+    //----------------------------------------//
+    // Write nuisance parameter values to file:
+    ofstream outFile(fileNameNP);
+    for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
+      outFile << paramNames[i_n] << " " << points[i_n] << " " << muLo[i_n] 
+	      << " " << muHi[i_n] << " " << valMu0[i_n] << " " << errLoMu0[i_n] 
+	      << " " << errHiMu0[i_n] << " " << valMu1[i_n] << " " 
+	      << errLoMu1[i_n] << " " << errHiMu1[i_n] << " " << valMuFree[i_n] 
+	      << " " << errLoMuFree[i_n] << " " << errHiMuFree[i_n] 
+	      << std::endl;
+    }
+    outFile.close();
     
-        
-    // Close the files that save CL data:
-    outFile_asym.close();
+    delete dhts;
+    delete workspace;
+    wsFile.Close();
+  }
+
+  //----------------------------------------//
+  // Rank the nuisance parameters by impact on mu:
+  std::cout << "DHNuisanceParameters: Rank NP according to mu impact."
+	      << std::endl;
+  std::vector<TString> sortedParamNames; sortedParamNames.clear();
+  std::vector<double> sortedParamValues; sortedParamValues.clear();
+  for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
+    double currMaxErr = TMath::Max(fabs(muLo[i_n]), fabs(muHi[i_n]));
+    if (i_n == 0) {
+      sortedParamNames.push_back(paramNames[i_n]);
+      sortedParamValues.push_back(currMaxErr);
+    }
+    else {
+      std::vector<TString>::iterator iterName = sortedParamNames.begin();
+      std::vector<double>::iterator iterVal= sortedParamValues.begin();
+      bool inserted = false;
+      while (iterName != sortedParamNames.end()) {
+	if (currMaxErr >= *iterVal) {
+	  sortedParamNames.insert(iterName, paramNames[i_n]);
+	  sortedParamValues.insert(iterVal, currMaxErr);
+	  inserted = true;
+	  break;
+	}
+	else {
+	  iterName++;
+	  iterVal++;
+	}
+      }
+      if (!inserted) {
+	sortedParamNames.push_back(paramNames[i_n]);
+	sortedParamValues.push_back(currMaxErr);
+      }
+    }
+  }
+  
+  // Finally, assign the rank of each NP:
+  for (int i_n = 0; i_n < (int)paramNames.size(); i_n++) {
+    for (int i_r = 0; i_r < (int)sortedParamNames.size(); i_n++) {
+      if ((sortedParamNames[i_r]).EqualTo(paramNames[i_n])) {
+	impactRank[i_n] = (int)(sortedParamNames.size()) - i_r;
+      }
+    }
   }
   
   //----------------------------------------//
   // Plot the results:
-    
+  std::cout << "DHNuisanceParameters: plot NP ranking results."
+	    << std::endl;
+  
   // Start plotting:
-  TCanvas *can = new TCanvas("can","can");
+  TCanvas *can = new TCanvas("can","can", 500, 800);
+  gStyle->SetPadLeftMargin(0.2);
   can->cd();
   
+  // Stupid histogram:
+  TH1F *hTemp = new TH1F("temp", "temp", (int)sortedParamNames.size()+5, 0,
+			 (int)sortedParamNames.size()+5);
+  for (int i_h = 0; i_h < (int)sortedParamNames.size()+5; i_h++) {
+    hTemp->SetBinContent(i_h+1, 0);
+    hTemp->GetXaxis()->SetBinLabel(i_h+1, sortedParamNames[i_h]);
+  }
+  hTemp->GetYaxis()->SetTitle("#delta#hat{#mu}");
+  hTemp->GetYaxis()->SetRangeUser(-0.25, 2.5);
+  hTemp->Draw("hbar");
+  
+  // Fill graphs with fit data:
+  double zeros[100] = {0.0};
+  TGraphAsymmErrors *gMuRank
+    = new TGraphAsymmErrors((int)paramNames.size(), zeros, impactRank, 
+			    muLo, muHi, impactRankErr, impactRankErr);
+  TGraphAsymmErrors *gFitMu0
+    = new TGraphAsymmErrors((int)paramNames.size(), valMu0, impactRank,
+			    errLoMu0, errHiMu0, zeros, zeros);
+  TGraphAsymmErrors *gFitMu1
+    = new TGraphAsymmErrors((int)paramNames.size(), valMu1, impactRank,
+			    errLoMu1, errHiMu1, zeros, zeros);
+  TGraphAsymmErrors *gFitMuFree
+    = new TGraphAsymmErrors((int)paramNames.size(), valMuFree, impactRank,
+			    errLoMuFree, errHiMuFree, zeros, zeros);
+  
+  //
+  gMuRank->SetFillColor(kOrange+1);
+  gMuRank->Draw("a2");
+  
+  // Print ATLAS text on the plot:    
+  TLatex t; t.SetNDC(); t.SetTextColor(kBlack);
+  t.SetTextFont(72); t.SetTextSize(0.05);
+  t.DrawLatex(0.44, 0.48, "ATLAS");
+  t.SetTextFont(42); t.SetTextSize(0.05);
+  t.DrawLatex(0.56, 0.48, config->getStr("ATLASLabel"));
+  t.DrawLatex(0.44, 0.42, Form("#sqrt{s} = 13 TeV, %2.1f fb^{-1}",
+			       (config->getNum("AnalysisLuminosity")/1000.0)));
+  
   // Legend:
-  TLegend leg(0.54,0.20,0.79,0.38);
+  TLegend leg(0.7,0.85,0.9,0.92);
   leg.SetBorderSize(0);
   leg.SetFillColor(0);
   leg.SetTextSize(0.04);
-  leg.AddEntry(gCLExp_toy,"Exp. limit","l");
-  leg.AddEntry(gCLExp_toy_1s,"Exp. limit #pm1#sigma_{exp}","F");
-  leg.AddEntry(gCLExp_toy_2s,"Exp. limit #pm2#sigma_{exp}","F");
+  leg.AddEntry(gMuRank, "Post-fit #hat{#mu} impact", "F");
+  leg.AddEntry(gFitMu0, "Pull, #mu=0", "LEP");
+  leg.AddEntry(gFitMu1, "Pull, #mu=1", "LEP");
+  leg.AddEntry(gFitMuFree, "#Pull, #hat{#mu}", "LEP");
   leg.Draw("SAME");
   
-  // 95% CL Line
+  // 95% CL Line:
+  /*
   TLine *line = new TLine();
   line->SetLineStyle(1);
   line->SetLineWidth(2);
@@ -229,34 +360,30 @@ int main(int argc, char **argv) {
     line->DrawLine(gCLExp_asym->GetXaxis()->GetXmin(), 0.95,
 		   gCLExp_asym->GetXaxis()->GetXmax(), 0.95);
   }
-  
-  // Print ATLAS text on the plot:    
-  TLatex t; t.SetNDC(); t.SetTextColor(kBlack);
-  t.SetTextFont(72); t.SetTextSize(0.05);
-  t.DrawLatex(0.54, 0.48, "ATLAS");
-  t.SetTextFont(42); t.SetTextSize(0.05);
-  t.DrawLatex(0.66, 0.48, config->getStr("ATLASLabel"));
-  t.DrawLatex(0.54, 0.42, Form("#sqrt{s} = 13 TeV, %2.1f fb^{-1}",
-			       (config->getNum("AnalysisLuminosity")/1000.0)));
-  
+  */
+    
   // Print the canvas:
-  can->Print(Form("%s/scan95CL_asymptotic_%s.eps",outputDir.Data(),tag.Data()));
-  can->Print(Form("%s/scan95CL_asymptotic_%s.C",outputDir.Data(),tag.Data()));
+  can->Print(Form("%s/plot_parameters_%s.eps", outputDir.Data(), tag.Data()));
+  can->Print(Form("%s/plot_parameters_%s.C", outputDir.Data(), tag.Data()));
   
-  // Delete pointers, close files, return:
-  std::cout << "DHNuisanceParameters: Finished!" << std::endl;
-  delete line;
+  // Save graphs to a ROOT file:
+  TFile rootFileOutput(Form("%s/nuisance_parameter_graphs_%s.root",
+			    outputDir.Data(), tag.Data()));
+  gMuRank->Write();
+  gFitMu0->Write();
+  gFitMu1->Write();
+  gFitMuFree->Write();
+  rootFileOutput.Close();
+  
+  // Print a warning if some fits failed:
+  if (!allGoodFits) {
+    std::cout << "\nDHNuisanceParameters: ERROR! Fits failed somewhere..."
+	      << "\n\t...queue Charlie Brown music" << std::endl;
+  }
+  else std::cout << "DHNuisanceParameters: Finished!" << std::endl;
+  
+  //delete line;
   delete can;
-  delete gCLObs_asym;
-  delete gCLExp_asym;
-  delete gCLObs_toy;
-  delete gCLExp_toy;
-  delete gCLExp_asym_2s;
-  delete gCLExp_asym_1s;
-  delete gCLExp_toy_2s;
-  delete gCLExp_toy_1s;
-  delete workspace;
   delete config;
-  wsFile.Close();
   return 0;
 }
